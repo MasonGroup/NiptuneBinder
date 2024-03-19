a6b885ad689cafb4c3db68c6e0a3f74586ba301a
Freemasonry <95870255+MasonGroup@users.noreply.github.com>
2024-03-19 10:00:46 -0700

    Add files via upload

---
+++ NiptuneBinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        MessageBox.Show("Gangsta freemasonry made me do it", "NiptuneBinder");
+        Environment.Exit(0);
+    }
+}
